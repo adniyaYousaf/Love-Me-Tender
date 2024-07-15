564ab4ca7fde2f74bbd8490e3321566a69fa473d
= <adniyayousaf92@gmail.com>
2024-07-15 21:50:59 +0000

    Set secure option to true for SSL

--- server/mail.js
+++ server/mail.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const transporter = nodemailer.createTransport({
     host: process.env.MAIL_HOST,
     port: process.env.MAIL_PORT,
-    secure: false,
+    secure: true,
     auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASSWORD,
