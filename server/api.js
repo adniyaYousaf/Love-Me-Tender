afaba66abedd3de3ab56df9371d61c3cda109077
= <adniyayousaf92@gmail.com>
2024-07-15 12:48:25 +0000

    Created a email system

--- server/api.js
+++ server/api.js
@@ -1,8 +1,25 @@
 import { Router } from "express";
-
+import mail from "./mail.js";
 import logger from "./utils/logger";
 
 const router = Router();
+
+router.post("/send-email", async (req, res) => {
+	const { email } = req.body;
+	if (!email) {
+		return res.status(400).send("Email is required.");
+	}
+	const subject = "New password";
+	const recipient = email;
+	const message = "Your passwors is ******.";
+	try {
+		await mail.sendEmail({ recipient, subject, message });
+		res.send("Email sent.");
+	} catch (error) {
+		logger.error("Error sending email:", error);
+		res.status(500).send("Failed to send email.");
+	}
+});
 
 router.get("/", (_, res) => {
 	logger.debug("Welcoming everyone...");
---
+++ server/mail.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-console */
+import nodemailer from "nodemailer";
+require("dotenv").config();
+const transporter = nodemailer.createTransport({
+    host: process.env.MAIL_HOST,
+    port: process.env.MAIL_PORT,
+    secure: false,
+    auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASSWORD,
+    },
+});
+
+const sendEmail = async ({ recipient, subject, message }) => {
+    return await transporter.sendMail({
+        from: "no_reply@example.com",
+        to: recipient,
+        subject,
+        text: message,
+        html: message,
+    });
+};
+
+
+
+export default { sendEmail };
