baa3a87de33e1ebcc175cf2bed248fcb4aeef068
= <adniyayousaf92@gmail.com>
2024-07-16 18:32:54 +0000

    fix the syntax

--- database/migrations/sqls/20240715163245-initial-db-up.sql
+++ database/migrations/sqls/20240715163245-initial-db-up.sql
@@ -44,7 +44,7 @@ CREATE TABLE tender (
     announcement_date DATE,
     deadline DATE,
     description VARCHAR(255),
-    cost DECIMAL(15, 2) CHECK (price >= 0),
+    cost DECIMAL(15, 2) CHECK (cost >= 0),
     status VARCHAR(100),
     last_update TIMESTAMP
 );
