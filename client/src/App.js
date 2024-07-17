9a54a8fbe4e5c60d711df8a66c3fb3e2ec1a02a8
= <adniyayousaf92@gmail.com>
2024-07-17 20:24:31 +0000

    added the files

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import PublishTenderForm from "./PublishTenderForm";
+import BuyerTenderList from "./BuyerTenderList/BuyerTenderList.jsx";
 
 const App = () => (
 	<Routes>
 		<Route path="/" element={<Home />} />
 		<Route path="/publish-tender" element={<PublishTenderForm />} />
+		<Route path="/BuyerTenderList" element={<BuyerTenderList />} />
 	</Routes>
 );
 
